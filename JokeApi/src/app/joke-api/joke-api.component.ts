22a6dce34a50283bbaf3e8e2f9ec929c3af6a1ae
OctagonalDefence <eromero110@boscdelacoma.cat>
2024-10-23 17:42:13 +0200

    JokeApiComponent

--- JokeApi/src/app/joke-api/joke-api.component.ts
+++ JokeApi/src/app/joke-api/joke-api.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { JokeApiService } from '../joke-api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-joke-api',
@@ -8,5 +10,27 @@ import { Component } from '@angular/core';
   styleUrl: './joke-api.component.scss'
 })
 export class JokeApiComponent {
+  joke: string = '';
+
+  constructor(private jokeApiService: JokeApiService) { }
+
+  ngOnInit(): void {
+    this.getJoke();
+  }
+
+  getJoke(): void {
+    this.jokeApiService.getJoke().subscribe(
+      data => {
+        if (data.type === 'single') {
+          this.joke = data.joke;
+        } else if (data.type === 'twopart') {
+          this.joke = `${data.setup} - ${data.delivery}`;
+        }
+      },
+      error => {
+        console.error('Error al obtenir la broma', error);
+      }
+    );
+  }
 
 }
