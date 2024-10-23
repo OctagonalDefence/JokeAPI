0fa06b88439f862c49dea98f1fe305fd1fd954da
OctagonalDefence <eromero110@boscdelacoma.cat>
2024-10-23 16:08:57 +0200

    generated component

---
+++ JokeApi/src/app/joke-api/joke-api.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { JokeApiComponent } from './joke-api.component';
+
+describe('JokeApiComponent', () => {
+  let component: JokeApiComponent;
+  let fixture: ComponentFixture<JokeApiComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [JokeApiComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(JokeApiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ JokeApi/src/app/joke-api/joke-api.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-joke-api',
+  standalone: true,
+  imports: [],
+  templateUrl: './joke-api.component.html',
+  styleUrl: './joke-api.component.scss'
+})
+export class JokeApiComponent {
+
+}
