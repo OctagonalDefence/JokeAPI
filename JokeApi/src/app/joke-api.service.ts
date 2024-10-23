199567045aac9432b95c6b8345011d3e916ec1fd
OctagonalDefence <eromero110@boscdelacoma.cat>
2024-10-23 16:52:52 +0200

    test api success

--- JokeApi/src/app/joke-api.service.ts
+++ JokeApi/src/app/joke-api.service.ts
@@ -1,10 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JokeApiService {
+
+  private apiUrl = 'https://v2.jokeapi.dev/joke/Any';
+
+  constructor(private http: HttpClient) { }
+
+  getJoke(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
  
 
-  constructor() { }
 }
